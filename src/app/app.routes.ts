581dccf5b23b8a5ff18898fbcf8a033ba7657f73
pradeep <solomon.terli@gmail.com>
2017-01-22 02:42:37 +0530

    angular 2 data grid component

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home';
+import { AboutComponent } from './about';
+import { NoContentComponent } from './no-content';
+
+import { DataResolver } from './app.resolver';
+
+export const ROUTES: Routes = [
+  { path: '',      component: HomeComponent },
+  { path: 'home',  component: HomeComponent },
+  { path: '**',    component: NoContentComponent },
+];
